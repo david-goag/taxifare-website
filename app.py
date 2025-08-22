a4e3dfd5b3bf554509424ade270b1118c58d63f9
David Gomez <davidgoag@gmail.com>
2025-08-22 15:07:32 +0200

    Updated

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import requests
 import pandas as pd
 
 import folium
-import streamlit as st
 
 from streamlit_folium import st_folium
 
@@ -12,49 +11,6 @@ from streamlit_folium import st_folium
 # TaxiFareModel front
 '''
 
-st.markdown('''
-
-Remember that there are several ways to output content into your web page...
-
-Either as with the title by just creating a string (or an f-string). Or as with this paragraph using the `st.` functions
-''')
-
-'''
-## Here we would like to add some controllers in order to ask the user to select the parameters of the ride
-
-1. Let's ask for:
-- date and time
-- pickup longitude
-- pickup latitude
-- dropoff longitude
-- dropoff latitude
-- passenger count
-'''
-
-'''
-## Once we have these, let's call our API in order to retrieve a prediction
-
-See ? No need to load a `model.joblib` file in this app, we do not even need to know anything about Data Science in order to retrieve a prediction...
-
-🤔 How could we call our API ? Off course... The `requests` package 💡
-'''
-
-url = 'https://taxifare-505391779697.europe-southwest1.run.app/'
-
-if url == 'https://taxifare-505391779697.europe-southwest1.run.app/':
-
-    st.markdown('Maybe you want to use your own API for the prediction, not the one provided by Le Wagon...')
-
-'''
-
-2. Let's build a dictionary containing the parameters for our API...
-
-3. Let's call our API using the `requests` package...
-
-4. Let's retrieve the prediction from the **JSON** returned by the API...
-
-## Finally, we can display the prediction to the user
-'''
 
 pickup_date = st.date_input("Select the travel day:", datetime.datetime.today())
 pickup_time = st.time_input('Select the pickup time', datetime.datetime.now())#, step=datetime.timedelta(0:15:00))
@@ -67,20 +23,20 @@ dropoff_longitude = -73.984365
 dropoff_latitude = 40.769802
 
 pickup_longitude_input = st.number_input('Select the Pickup Longitude:', format="%0.6f", value=pickup_longitude)
-st.write('Pickup Longitude: ', pickup_longitude_input)
+#st.write('Pickup Longitude: ', pickup_longitude_input)
 
-passenger_count = st.slider('Select a pickup_datetime', 1, 8, 2)
-st.write("Number of passengers: ", passenger_count)
+pickup_latitude_input = st.number_input('Select the Pickup Latitude:', format="%0.6f", value=pickup_latitude)
+#st.write('Pickup Latitude: ', pickup_latitude_input)
 
+dropoff_longitude_input = st.number_input('Select the Dropoff Longitude:', format="%0.6f", value=dropoff_longitude)
+#st.write('Dropoff Longitude: ', dropoff_longitude_input)
 
+dropoff_latitude_input = st.number_input('Select the Dropoff Latitude:', format="%0.6f", value=dropoff_latitude)
+#st.write('Dropoff Latitude: ', dropoff_latitude_input)
 
+passenger_count = st.slider('Select the number of passengers', 1, 8, 2)
+#st.write("Number of passengers: ", passenger_count)
 
-url = 'https://taxifare-505391779697.europe-southwest1.run.app/predict?'
-
-pickup_longitude = -73.950655
-pickup_latitude = 40.783282
-dropoff_longitude = -73.984365
-dropoff_latitude = 40.769802
 
 #pickup_df = pd.DataFrame({'lon': [pickup_longitude], 'lat': [pickup_latitude]})
 #dropoff_df = pd.DataFrame({'lon': [dropoff_longitude], 'lat': [dropoff_latitude]})
@@ -88,17 +44,8 @@ map_df = pd.DataFrame({'lon': [pickup_longitude, dropoff_longitude], 'lat': [pic
 
 #st.map(pickup_df)
 #st.map(dropoff_df, color="#ffff00")
-#st.map(map_df, color=map_df["color"])
 
-
-
-"""# center on Liberty Bell, add marker
-m = folium.Map(location=[39.949610, -75.150282], zoom_start=16)
-folium.Marker(
-    [39.949610, -75.150282]#[39.949610, -75.150282], popup="Liberty Bell", tooltip="Liberty Bell"
-).add_to(m)"""
-
-m = folium.Map(location=[(pickup_latitude+dropoff_latitude)/2, (pickup_longitude+dropoff_longitude)/2], zoom_start=12)
+m = folium.Map(location=[(pickup_latitude+dropoff_latitude)/2, (pickup_longitude+dropoff_longitude)/2], zoom_start=14)
 for i in range(0,len(map_df)):
    folium.Marker(
       location=[map_df.iloc[i]['lat'], map_df.iloc[i]['lon']],
@@ -112,15 +59,21 @@ for i in range(0,len(map_df)):
 # call to render Folium map in Streamlit
 st_data = st_folium(m, width=725)
 
+
+url = 'https://taxifare-505391779697.europe-southwest1.run.app/predict?'
+
 params = {
         "pickup_datetime":pickup_datetime,
-        "pickup_longitude":pickup_longitude,
-        "pickup_latitude":pickup_latitude,
-        "dropoff_longitude":dropoff_longitude,
-        "dropoff_latitude":dropoff_latitude,
+        "pickup_longitude":pickup_longitude_input,
+        "pickup_latitude":pickup_latitude_input,
+        "dropoff_longitude":dropoff_longitude_input,
+        "dropoff_latitude":dropoff_latitude_input,
         "passenger_count":passenger_count
     }
 
 response = requests.get(url, params=params)
 
-st.write("Fare: ", response.json()["fare"])
+#st.write("Fare: ", response.json()["fare"])
+f'''
+# Me debes {round(response.json()["fare"], 2)}$
+'''
